775f087d62f404bcfd400f551b3dae41afda2412
Thomas Kiley <thomas.kiley@diffblue.com>
2019-10-28 12:40:14 +0000

    Fix unit tests

--- test/test_ward_newsletter_name.py
+++ test/test_ward_newsletter_name.py
@@ -6,5 +6,6 @@ from ward_newsletter import ward_newsletter_name
 
 class TestWard_newsletter_name(TestCase):
     def test_ward_newsletter_name(self):
-        self.assertEqual(ward_newsletter_name(datetime.date(1900, 1, 1)), "January 1900 Ward Newsletter")
-        self.assertEqual(ward_newsletter_name(datetime.date(2019, 10, 1)), "October 2019 Ward Newsletter")
+        self.assertEqual(ward_newsletter_name(datetime.date(1900, 1, 1), 'Cowley'),
+                         "Cowley January 1900 Ward Newsletter")
+        self.assertEqual(ward_newsletter_name(datetime.date(2019, 10, 1), 'OST'), "OST October 2019 Ward Newsletter")
