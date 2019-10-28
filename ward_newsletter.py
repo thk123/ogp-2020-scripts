4ad61342be8ac55a1d6bebb84a4a2947f65b89c4
Thomas Kiley <thomas.kiley@diffblue.com>
2019-10-28 17:40:22 +0000

    Create a student leaflet trello chain
    Includes shorter delivery time, longer time to arrange delivery and also
    a warning to Hillingdon that we will need to print something

--- ward_newsletter.py
+++ ward_newsletter.py
@@ -29,6 +29,21 @@ def produce_leaflet_dependency(start_delivery_date):
     return Prereq("Print Leaflets", start_delivery_date, start_printing, [design_leaflet])
 
 
+def produce_student_leaflet_dependencies(start_delivery_date):
+    warn_hillingdon = datetime.timedelta(days=28)
+    collect_data_time = datetime.timedelta(days=21)
+    print_window = datetime.timedelta(days=7)
+
+    start_printing = start_delivery_date - print_window
+    design_window = datetime.timedelta(days=21)
+    design_leaflet = Prereq("Design leaflet", start_printing, start_printing - design_window, [])
+    collect_register = Prereq("Collect up to date register", start_printing, start_printing - collect_data_time, [])
+    warn_hgp = Prereq("Warn Hillingdon Green Print of expected print", start_printing, start_printing - warn_hillingdon,
+                      [])
+
+    return Prereq("Print Leaflets", start_delivery_date, start_printing, [design_leaflet, warn_hgp, collect_register])
+
+
 def leaflet_dependencies(delivery_date):
     delivery_window = datetime.timedelta(days=14)
     start_delivery = delivery_date - delivery_window
@@ -39,6 +54,21 @@ def leaflet_dependencies(delivery_date):
                   delivery_date,
                   start_delivery,
                   [produce_leaflet_dependency(start_delivery),
+                   Prereq("Prepare delivery rounds", start_delivery, start_delivery - round_prep_window, [])
+                   ])
+
+
+def student_leaflet_dependecies(delivery_date):
+    delivery_window = datetime.timedelta(days=7)
+    start_delivery = delivery_date - delivery_window
+
+    # allow for more time - need to create an action day for students to come help out
+    round_prep_window = datetime.timedelta(days=21)
+
+    return Prereq("Deliver leaflets",
+                  delivery_date,
+                  start_delivery,
+                  [produce_student_leaflet_dependencies(start_delivery),
                    Prereq("Prepare delivery rounds", start_delivery, start_delivery - round_prep_window, [])
                    ])
 
@@ -55,24 +85,49 @@ def add_prereqs(card, prereqs, board, goal_label, list):
         prereq_card.set_due(prereq.due_date)
         prereq_card.add_label(goal_label)
         prereq_card.set_description("Start work: " + str(prereq.start_date) + '\n\n' + prereq.desc)
-        add_prereqs(prereq_card, prereq.prereqs, board, goal_label, list)
+        add_prereqs(card, prereq.prereqs, board, goal_label, list)
         dependency_cards.append(prereq_card.url)
         pos = datetime.datetime.combine(prereq.start_date, datetime.time(12, 0)).timestamp()
         prereq_card.set_pos(pos)
 
-    card.add_checklist('TODO', dependency_cards)
+    found = False
+    for c in card.checklists:
+        if c.name == 'TODO':
+            found = True
+            for item in dependency_cards:
+                c.add_checklist_item(item)
+            break
+    if not found:
+        card.add_checklist('TODO', dependency_cards)
 
 
-def create_ward_newsletter(delivery_date, board, prefix):
-    goal_name = ward_newsletter_name(delivery_date, prefix)
+def create_ward_newsletter(delivery_date, board, prefix, custom_name=None):
+    if not custom_name:
+        custom_name = ward_newsletter_name(delivery_date, prefix)
     backlog = trello_utility.get_list("Backlog", board)
-    goal_card = backlog.add_card(goal_name)
+    goal_card = backlog.add_card(custom_name)
     goal_card.add_label(trello_utility.get_label('Goal', board))
 
-    goal_label = trello_utility.create_label(goal_name, "null", board)
+    goal_label = trello_utility.create_label(custom_name, trello_utility.random_goal_colour(), board)
     goal_card.add_label(goal_label)
     goal_card.set_due(delivery_date)
 
     prereqs = [leaflet_dependencies(delivery_date)]
+    add_prereqs(goal_card, prereqs, board, goal_label, backlog)
+    position_card(board, goal_card)
+
+
+def create_student_leaflet(delivery_date, board, prefix, custom_name=None):
+    if not custom_name:
+        custom_name = ward_newsletter_name(delivery_date, prefix)
+    backlog = trello_utility.get_list("Backlog", board)
+    goal_card = backlog.add_card(custom_name)
+    goal_card.add_label(trello_utility.get_label('Goal', board))
+
+    goal_label = trello_utility.create_label(custom_name, trello_utility.random_goal_colour(), board)
+    goal_card.add_label(goal_label)
+    goal_card.set_due(delivery_date)
+
+    prereqs = [student_leaflet_dependecies(delivery_date)]
     add_prereqs(goal_card, prereqs, board, goal_label, backlog)
     position_card(board, goal_card)
