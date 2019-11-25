576df10e433d1ba10ecd885b630141d73609c9a3
Thomas Kiley <thomas.kiley@diffblue.com>
2019-11-25 13:45:12 +0000

    Updating the data analysis script to work with the gmap lookup

--- survey_count.py
+++ survey_count.py
@@ -1,13 +1,16 @@
 import csv
+import os
 import re
 import sys
 from enum import Enum
 
+import gmap_lookup
+from gmap_lookup import GMapIdLookup
+
 
 class Address:
-    def __init__(self, road, house_number):
-        self.road = road
-        self.house_number = house_number
+    def __init__(self, search_string):
+        self.search_string = search_string
 
     def __eq__(self, other):
         """Overrides the default implementation"""
@@ -42,14 +45,14 @@ class Voted(Enum):
     PV = 2
 
 
-def parse_marked_register_row(row):
+def parse_marked_register_row(row, address_ider: GMapIdLookup):
     address = row["address"]
     voted = int(row["vote2018"])
 
     if voted not in set(item.value for item in Voted):
         raise Exception('Unexpected voting number in "voted2018": ' + str(voted))
 
-    return Address.from_full_string(address), Voted(voted)
+    return address_ider.id_of_place(address + ', Oxford'), Voted(voted)
 
 
 class MarkedRegisterStats:
@@ -64,16 +67,18 @@ class MarkedRegisterStats:
                                                                      self.postal_voters, self.non_voters)
 
 
-def parse_marked_register(path):
+def parse_marked_register(path, address_ider):
     results = {}
     stats = MarkedRegisterStats()
+
     with open(path, 'r') as csv_file:
         marked_register = csv.DictReader(csv_file)
         for row in marked_register:
             print(row)
             voted: Voted
-            address, voted = parse_marked_register_row(row)
-            results[address] = voted
+            address, voted = parse_marked_register_row(row, address_ider)
+            if address not in results or (address in results and results[address] == Voted.DNV):
+                results[address] = voted
             stats.marked_reg_entries += 1
 
             if voted == Voted.DNV:
@@ -86,20 +91,101 @@ def parse_marked_register(path):
     return results, stats
 
 
+def id_from_road_and_number(road_name, house_number, address_ider):
+    return address_ider.id_of_place(address_search_string(house_number, road_name))
+
+
+def address_search_string(house_number, road_name):
+    return house_number + ' ' + road_name + ', Oxford'
+
+
+def parse_survey_data(path, address_ider):
+    results = []
+    with open(path, 'r') as csv_file:
+        survey_response = csv.reader(csv_file)
+        road_name = ''
+        for row in survey_response:
+            if row[0] and row[0] == 'Road name':
+                continue
+            if row[0] and row[0] != '':
+                road_name = row[0]
+                print('Reading road: ' + road_name)
+            if row[1] and row[1] != '':
+                house_number = row[1]
+                try:
+                    id = id_from_road_and_number(road_name, house_number, address_ider)
+                except gmap_lookup.InvalidPlaceException:
+                    print('Unable to map location: ' + address_search_string(house_number, road_name))
+                print(road_name + house_number)
+                results.append(id)
+
+    return results
+
+
+def analyse_data(marked_register, survey_responses):
+    pass
+
 def main():
     if len(sys.argv) < 2:
         print("Expected: <marked_register> <survey_response>[, <survey_response>...]")
 
+    address_ider = GMapIdLookup(os.environ['gmap_key'])
+    cache_path = 'gmap_cache.csv'
+    address_ider.load_cache(cache_path)
     marked_register_path = sys.argv[1]
-    marked_register, marked_reg_stats = parse_marked_register(marked_register_path)
-    print(str(marked_reg_stats))
-    # totals
+    marked_register = {}
+    try:
+        pass
+        marked_register, marked_reg_stats = parse_marked_register(marked_register_path, address_ider)
+        print(str(marked_reg_stats))
 
-    survey_responses = 0
+    finally:
+        pass
+        address_ider.save_cache(cache_path)
+
+    results = []
+    try:
+        for survey_path in sys.argv[2:]:
+            results.extend(parse_survey_data(survey_path, address_ider))
+    finally:
+        pass
+        address_ider.save_cache(cache_path)
+
+    survey_count = len(results)
     surveyed_postal = 0
     surveyed_voted = 0
     surveyed_non_on_reg = 0
-    surveyde_dnv = 0
+    surveyed_dnv = 0
+    for survey_response in results:
+        if survey_response in marked_register:
+            voted = marked_register[survey_response]
+            if voted == Voted.DNV:
+                surveyed_dnv += 1
+            elif voted == Voted.PV:
+                surveyed_postal += 1
+            elif voted == Voted.VOTED:
+                surveyed_voted += 1
+        else:
+            surveyed_non_on_reg += 1
+
+    spoken_to = 0
+    not_spoken_to = 0
+    for voter in marked_register:
+        if marked_register[voter] == Voted.PV or marked_register[voter] == Voted.VOTED:
+            if voter in results:
+                spoken_to += 1
+            else:
+                not_spoken_to += 1
+
+    print('Spoken to: ' + str(spoken_to))
+    print('Not spoken to: ' + str(not_spoken_to))
+
+    print('surveyed_postal = ' + str(surveyed_postal))
+    print('surveyed_voted = ' + str(surveyed_voted))
+    print('surveyed_non_on_reg = ' + str(surveyed_non_on_reg))
+    print('surveyed_dnv = ' + str(surveyed_dnv))
+    print('survey_responses = ' + str(survey_count))
+
 
 
 if __name__ == "__main__":
